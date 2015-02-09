b6aaefe27b08d98688101bd5fe1e40e73cf33e9a
Sam Davies <samdavies@live.co.uk>
2015-02-09 09:13:33 +0000

    added home

--- big-mess.py
+++ big-mess.py
@@ -1,12 +1,12 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template("base.html")
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
