27067aa14e4199bf90e5ed4f3c447f0c145a28eb
Sam Davies <samdavies@live.co.uk>
2015-02-09 09:17:14 +0000

    added all pages

--- big-mess.py
+++ big-mess.py
@@ -5,7 +5,27 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template("base.html")
+    return render_template("home.html")
+
+
+@app.route('/contacts')
+def hello_world():
+    return render_template("contacts.html")
+
+
+@app.route('/portfolio')
+def hello_world():
+    return render_template("portfolio.html")
+
+
+@app.route('/services')
+def hello_world():
+    return render_template("services.html")
+
+
+@app.route('/about_us')
+def hello_world():
+    return render_template("about_us.html")
 
 
 if __name__ == '__main__':
